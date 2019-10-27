cce2a209839ad65d55f94c32fedcd8b6e071e1a2
unknown <artur0311@hotmail.com>
2019-10-27 10:39:30 -0200

    App de todos: app completo com funções de excluir, adicionar e salvar todos

--- JAVASCRIPT/controle.js
+++ JAVASCRIPT/controle.js
@@ -2,21 +2,28 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todos = [
-    'Ir a festa',
-    'Fazer café',
-    'Estudar termodinamica',
-    'Estudar javascript',
-]
+var todos = JSON. parse (localStorage.getItem ('list_todos')) || [];
 
 function renderTodos(){
-    listElement.innerHTML ='';
+    listElement.innerHTML = '' ;
 
     for (todo of todos){
         var todoElement = document.createElement ('li');
         var todoText = document.createTextNode (todo);
+        var linkElement = document.createElement ('a');
 
+        linkElement.setAttribute('href', '#');
+
+        var pos = todos.indexOf (todo);
+        linkElement.setAttribute ('onclick', 'deleteTodo(' + pos + ')');
+
+        var linkText = document.createTextNode (' Excluir');
+
+        linkElement.appendChild (linkText);
+      
         todoElement.appendChild (todoText);
+        todoElement.appendChild (linkElement);
+
         listElement.appendChild (todoElement);
     }
 }
@@ -28,6 +35,17 @@ function addTodo(){
     todos.push (todoText);
     inputElement.value = '';
     renderTodos();
+    saveToStorage();
 }
 
 buttonElement.onclick = addTodo;
+
+function deleteTodo(pos){
+    todos.splice (pos, 1);
+    renderTodos();
+    saveToStorage();
+}
+
+function saveToStorage(){
+    localStorage.setItem('list_todos', JSON.stringify(todos));
+}
